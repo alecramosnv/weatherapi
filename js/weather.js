4bd42504499fbc26c64e9da4be95d637f556b83c
Alec Ramos <Alec Ramos>
2016-05-02 16:20:42 -0700

    Add Air Quality

--- js/weather.js
+++ js/weather.js
@@ -64,25 +64,24 @@ $(document).ready(function(){
 	}
 
 	function weather (event) {
+		forecast();
+		currentWeather();
+		air();
+	}
 
+
+	function currentWeather (event) {
+		var code = $('#code').val();
 		if (key != null) {
-			var code = $('#code').val();
 			$.getJSON('http://api.openweathermap.org/data/2.5/weather?zip=' + code + ',us&appid=' + key + '&units=imperial', function(data){
 				$('#results').show();
 				$('.location').text(data.name);
 				$('#country').text(', ' + data.sys.country);
 				$('#temp').text(data.main.temp + ' °F');
-				$('#temp_max').text(data.main.temp_max + ' °F');
-				$('#temp_min').text(data.main.temp_min + ' °F');
 				$('#humidity').text(data.main.humidity + '%');
 				$('#pressure').text(data.main.pressure + ' HPA');
 				$('#clouds').text(data.weather[0].description);
 				$('#wind').text(data.wind.speed + '  mph from the ' + degToCompass(data.wind.deg));
-				$('#weather').text(data.weather[0].main);
-				var conditions = $('#weather').text();
-				if (conditions == null) {
-					$('#conditions').hide();
-				}
 				$('#sunrise').text(formatTime(data.sys.sunrise));
 				$('#sunset').text(formatTime(data.sys.sunset));
 				$('#coord').text('Latitude: ' + data.coord.lat + ', Longitude: ' + data.coord.lon);
@@ -93,5 +92,47 @@ $(document).ready(function(){
 			})
 		}
 	}
+
+	function forecast (event) {
+		var code = $('#code').val();
+		$.getJSON('http://api.openweathermap.org/data/2.5/weather?zip=' + code + ',us&appid=' + key + '&units=imperial', function(data){
+			var city = data.name;
+			$.getJSON('http://api.openweathermap.org/data/2.5/forecast/daily?q=' + city + '&units=imperial&cnt=7&appid=' + key, function(forecast){
+				$('#temp_max').text(forecast.list[0].temp.max + ' °F');
+				$('#temp_min').text(forecast.list[0].temp.min + ' °F');
+				$('#icon').attr('src', 'http://openweathermap.org/img/w/' + forecast.list[0].weather[0].icon + '.png');
+				var id = forecast.city.id;
+			})
+		})
+	}
+
+	var key2 = getCookie("key2");
+	if (key2 == "") {
+		key2 = prompt("Please enter your second key:", "");
+		if (key2 != "" && key2 != null) {
+			setCookie("key2", key2, 365);
+		}
+	}
+
+	function air (event) {
+		var code = $('#code').val();
+		$.getJSON('http://api.openweathermap.org/data/2.5/weather?zip=' + code + ',us&appid=' + key + '&units=imperial', function(data){
+			var city = data.name;
+			var lat = data.coord.lat;
+			var lon = data.coord.lon;
+			$.getJSON('https://api.breezometer.com/baqi/?lat=' + lat + '&lon=' + lon + '&key=' + key2, function(air){
+				$('#air').text(air.breezometer_description);
+				$('#air').css('color', air.breezometer_color);
+				$('#children').text(air.random_recommendations.children);
+				$('#health').text(air.random_recommendations.health);
+				$('#inside').text(air.random_recommendations.inside);
+				$('#outside').text(air.random_recommendations.outside);
+				$('#sport').text(air.random_recommendations.sport);
+			})
+		})
+	}
+
+	// first key: 01671288806d7d8df40c5f847b77021c
+	// second key: 07dd79e6867f4949b83c77b59fedfe81
 
 });
