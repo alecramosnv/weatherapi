1c0854ed60de41635415cbeebf918fa104176fa4
Alec Ramos <alec1034780@icloud.com>
2016-05-06 18:56:33 -0700

    Add Files

---
+++ js/controllers/MainController.js
@@ -0,0 +1,22 @@
+angular.module('root', [])
+
+.controller("index", ["$scope", function ($scope) {
+
+	$scope.temp = "Hello World!";
+	$scope.temp_max = "Hello World!";
+	$scope.temp_min = "Hello World!";
+	$scope.wind = "Hello World!";
+	$scope.clouds = "Hello World!";
+	$scope.pressure = "Hello World!";
+	$scope.air = "Hello World!";
+	$scope.humidity = "Hello World!";
+	$scope.coord = "Hello World!";
+	$scope.sunrise = "Hello World!";
+	$scope.sunset = "Hello World!";
+	$scope.children = "Hello World!";
+	$scope.inside = "Hello World!";
+	$scope.outside = "Hello World!";
+	$scope.sport = "Hello World!";
+	$scope.health = "Hello World!";
+
+}]);
